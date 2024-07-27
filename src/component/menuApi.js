207f4c47ac0cb5c36e58e1f8207d1ee512b3a221
Yagnen27 <yagnen27barot@gmail.com>
2024-07-27 17:39:01 +0530

    Initial commit

---
+++ src/component/menuApi.js
@@ -0,0 +1,136 @@
+const Menu = [
+
+    {
+        id: 1,
+        image: "images/pizza3.jpg",
+        name: "Blazing Paprika",
+        category: "pizza",
+        price: 329,
+        description: "Hot & spicy pizza with onion & red paprika toppings and a new spicy peri peri sauce on a Domino's cheesy base.",
+
+    },
+
+    {
+        id: 2,
+        image: "images/pizza4.jpg",
+        name: "Veg Extravaganza",
+        category: "pizza",
+        price: 549,
+        description: "Black olives, capsicum, onion, grilled mushroom, corn, tomato, jalapeno & extra cheese",
+
+    },
+
+    {
+        id: 3,
+        image: "images/pizza5.jpg",
+        name: "Veggie Buddies",
+        category: "pizza",
+        price: 379,
+        description: "Best buddies Tomato and Capsicum come together on your Fav Crust with cheese & a no onion no garlic sauce",
+
+    },
+
+    {
+        id: 4,
+        image: "images/pizza1.jpg",
+        name: "Cheese Overload",
+        category: "pizza",
+        price: 639,
+        description: "Cheese Overloaded Pizza with a medley of Mozzarella, Cheddar & Ghost Pepper flavoured Cheese.",
+
+    },
+
+    {
+        id: 5,
+        image: "images/pizza2.jpg",
+        name: "Burn To Hell",
+        category: "pizza",
+        price: 459,
+        description: "A fiery and lethal combination of hot &amp; garlic dip, jalapenos, mushrooms, olives and capsicum",
+
+    },
+
+    {
+        id: 6,
+        image: "images/beverage1.jpg",
+        name: "Pepsi 295 ml",
+        category: "beverage",
+        price: 49,
+        description: "A fiery and lethal combination of hot &amp; garlic dip, jalapenos, mushrooms, olives and capsicum",
+    },
+
+    {
+        id: 7,
+        image: "images/beverage2.jpg",
+        name: "Iced Beer 475ml",
+        category: "beverage",
+        price: 60,
+        description: "Delicious Orange Flavoured beverage",
+    },
+
+    {
+        id: 8,
+        image: "images/beverage1.jpg",
+        name: "Pepsi 475ml",
+        category: "beverage",
+        price: 60,
+        description: "Sparkling and Refreshing Beverage",
+    },
+
+    {
+        id: 9,
+        image: "images/combo1.jpg",
+        name: "Meal for 2: Margherita & Farmhouse",
+        category: "combos",
+        price: 459,
+        description: "Regular Margherita +Regular Farmhouse + Garlic Bread + Pepsi",
+    },
+
+    {
+        id: 10,
+        image: "images/combo2.jpg",
+        name: "Meal for 2: Farmhouse & Peppy Paneer",
+        category: "combos",
+        price: 589,
+        description: "Regular Farmhouse + Regular Peppy Paneer + Garlic Bread + Pepsi",
+    },
+
+    {
+        id: 11,
+        image: "images/combo3.jpg",
+        name: "Meal for 4: Peppy Paneer & Farmhous",
+        category: "combos",
+        price: 969,
+        description: "Medium Peppy Paneer + Medium Farmhouse + Garlic Bread + 2 Pepsi",
+    },
+
+    {
+        id: 12,
+        image: "images/desert1.jpg",
+        name: "2 Berry Mix Pastry",
+        category: "dessert",
+        price: 159,
+        description: "Cherry +  Raspberries Mixed In Pastry",
+    },
+
+    {
+        id: 13,
+        image: "images/desert2.jpg",
+        name: "Strawberry Ice-Cream",
+        category: "dessert",
+        price: 249,
+        description: "Strawberry Ice-Cream With Strawberry Cuts",
+    },
+
+    {
+        id: 14,
+        image: "images/desert3.jpg",
+        name: "3 Chocolate Pastries",
+        category: "dessert",
+        price: 699,
+        description: "Chocolate lovers delight! Indulgent, gooey molten lava inside chocolate cake",
+    },
+
+];
+
+export default Menu;
